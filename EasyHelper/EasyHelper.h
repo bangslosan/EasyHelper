9f7c65b9a24436c119d8dc72fe925e0baa33958c
DaRkD0G <postmaster@yannickstephan.com>
2015-09-30 11:10:18 +0200

    some init msg

---
+++ EasyHelper/EasyHelper.h
@@ -0,0 +1,19 @@
+//
+//  EasyHelper.h
+//  EasyHelper
+//
+//  Created by DaRk-_-D0G on 30/09/2015.
+//  Copyright © 2015 DaRk-_-D0G. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EasyHelper.
+FOUNDATION_EXPORT double EasyHelperVersionNumber;
+
+//! Project version string for EasyHelper.
+FOUNDATION_EXPORT const unsigned char EasyHelperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EasyHelper/PublicHeader.h>
+
+
