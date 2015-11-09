df7f07ce0801b424a855e8383b536707697a26ae
DaRkD0G <postmaster@yannickstephan.com>
2015-11-09 18:27:05 +0100

    Up

---
+++ EasyHelperWatch/EasyHelperWatch.h
@@ -0,0 +1,19 @@
+//
+//  EasyHelperWatch.h
+//  EasyHelperWatch
+//
+//  Created by DaRk-_-D0G on 15/10/2015.
+//  Copyright © 2015 DaRk-_-D0G. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for EasyHelperWatch.
+FOUNDATION_EXPORT double EasyHelperWatchVersionNumber;
+
+//! Project version string for EasyHelperWatch.
+FOUNDATION_EXPORT const unsigned char EasyHelperWatchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EasyHelperWatch/PublicHeader.h>
+
+
